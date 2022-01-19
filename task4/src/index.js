ad74ab6d04671f4eb10fa928260c3b138dfa8fff
phoenix979797 <phoenix979797@proton.me>
2022-01-19 14:37:36 +0200

    cc

--- task4/src/index.js
+++ task4/src/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 
+
 const rootElement = document.querySelector("#root");
 const renderGreeting = (elem) => {
   const element = (
