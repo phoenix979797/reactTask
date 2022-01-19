02da0ab8de8bf6529462f88419370ab81ed6af3e
phoenix979797 <phoenix979797@proton.me>
2022-01-19 14:29:29 +0200

    God save gromecode

--- todo-list/src/index.js
+++ todo-list/src/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 
+
 const rootElement = document.querySelector("#root");
 const renderGreeting = (elem) => {
   const element = <h1 class="title">Todo List</h1>;
