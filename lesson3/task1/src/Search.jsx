c0ce8612f71f4e6d87ea1fbecc7ed90b4981c8a7
phoenix979797 <phoenix979797@proton.me>
2022-01-20 11:55:18 +0200

    sad

--- lesson3/task1/src/Search.jsx
+++ lesson3/task1/src/Search.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-export const Search = (props) => {
+export default Search = (props) => {
     return (<div className="search">
         <h1 className="search__title">{`Hello, ${props.name}. What to search for you?`}</h1>
         <div className="search__field">
