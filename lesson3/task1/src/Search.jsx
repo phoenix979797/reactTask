f39d7993bc95deba4f2a6ec3c99cf89ae6fe18a8
phoenix979797 <phoenix979797@proton.me>
2022-01-20 15:54:51 +0200

    dsa

--- lesson3/task1/src/Search.jsx
+++ lesson3/task1/src/Search.jsx
@@ -1,12 +1,19 @@
-import React from "react";
-const Search = (props) => {
-    return (<div className="search">
-        <h1 className="search__title">{`Hello, ${props.name}. What to search for you?`}</h1>
-        <div className="search__field">
-            <input type="text" className="search__input" />
-            <button className="search__button">Search</button>
-        </div>
-    </div>)
-};
+import React, { Component } from "react";
+// const Search = (props) => {
+//     return
+// };
+
+class Search extends Component {
+    render() {
+        return (<div className="search">
+            <h1 className="search__title">{`Hello, ${this.props.name}. What to search for you?`}</h1>
+            <div className="search__field">
+                <input type="text" className="search__input" />
+                <button className="search__button">Search</button>
+            </div>
+        </div>)
+    }
+
+}
 
 export default Search;
