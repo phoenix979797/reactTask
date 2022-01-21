4c96a1712a0db8bddb8594d22efc5257a78786ea
phoenix979797 <phoenix979797@proton.me>
2022-01-21 14:42:29 +0200

    ддд

--- lesson3/task2/src/Profile.jsx
+++ lesson3/task2/src/Profile.jsx
@@ -10,7 +10,7 @@ function Profile(props) {
             <div className="profile__name">
                 {`${props.userData.firstName} ${props.userData.lastName}`}
             </div>
-            <div className="profile__bitrth">
+            <div className="profile__birth">
                 {`Was born ${formatDate(props.userData.birthDate)} in ${props.userData.birthPlace}`}
             </div>
         </>
