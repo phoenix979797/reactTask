f98e95b05de0dc97290e6f8232c923af82c3214e
phoenix979797 <phoenix979797@proton.me>
2022-01-24 13:00:52 +0200

    sad

--- lesson4/task1/src/index.jsx
+++ lesson4/task1/src/index.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import Counter from "./Counter";
+import Counter from "./Colors";
 import './index.scss';
 
 const rootElement = document.querySelector('#root');
