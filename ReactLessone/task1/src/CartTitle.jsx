44459875040893a63de925ebd6bdc9fa1d50d98d
phoenix979797 <phoenix979797@proton.me>
2022-02-08 11:29:12 +0200

    asd

--- ReactLessone/task1/src/CartTitle.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const CartTitle = ({ userName, count }) => (
-    <div className="cart-title">{`${userName}, you added ${count} items`}</div>
-);
-
-export default CartTitle;
--- ReactLessone/task1/src/index.jsx
+++ ReactLessone/task1/src/index.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.scss';
-import App from './App.jsx';
+import App from './Calculator.jsx';
 
 
 
 
 const rootElement = document.querySelector('#root');
 
-ReactDOM.render(<App userId="facebook" />, rootElement);
+ReactDOM.render(<App  />, rootElement);
 
