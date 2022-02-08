65ce019e0adb6aad563fa9ea43ead60d0d669c92
phoenix979797 <phoenix979797@proton.me>
2022-02-08 18:12:19 +0200

    sad

--- ReactLessone/task1/src/index.jsx
+++ ReactLessone/task1/src/index.jsx
@@ -1,19 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.scss';
-import App from './FilterableProductTable.jsx';
+import App from './App.jsx';
 
 
-const PRODUCTS = [
-    { category: 'Sporting Goods', price: '$49.99', stocked: true, name: 'Football' },
-    { category: 'Sporting Goods', price: '$9.99', stocked: true, name: 'Baseball' },
-    { category: 'Sporting Goods', price: '$29.99', stocked: false, name: 'Basketball' },
-    { category: 'Electronics', price: '$99.99', stocked: true, name: 'iPod Touch' },
-    { category: 'Electronics', price: '$399.99', stocked: false, name: 'iPhone 5' },
-    { category: 'Electronics', price: '$199.99', stocked: true, name: 'Nexus 7' }
-];
 
 const rootElement = document.querySelector('#root');
 
-ReactDOM.render(<App products={PRODUCTS} />, rootElement);
+ReactDOM.render(<App />, rootElement);
 
