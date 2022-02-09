df50e9cc335b738b4c3536cc716e51629d44c5b4
phoenix979797 <phoenix979797@proton.me>
2022-02-09 12:50:04 +0200

    asdsad

--- ReactLessone/task1/src/App.jsx
+++ ReactLessone/task1/src/App.jsx
@@ -1,37 +1,17 @@
-import React, { Component } from "react";
-import Dialog from "./Dialog";
+import React from 'react';
+import Expand from './Expand';
 
-
-
-class App extends Component {
-
-    state = {
-        isOpen: false
-    }
-
-    hideDialog = () => {
-        this.setState({
-            isOpen: false
-        })
-    }
-
-    openDialog = () => {
-        this.setState({
-            isOpen: true
-        })
-    }
-
-    render() {
-        const elem = (
-            <p>Use immutable array methods to work with data. It will help to avoid bugs</p>
-        )
-        return (
-            <div className="app">
-                <button className="btn" onClick={this.openDialog}>Show dialog</button>
-                <Dialog onClose={this.hideDialog} title="some text" isOpen={this.state.isOpen}>{elem}</Dialog>
-            </div>
-        )
-    }
-}
+const App = () => {
+    return (
+        <div className="app">
+            <Expand title="Some title">
+                <p>
+                    Hooks are a new addition in React 16.8. They let you use state and
+                    other React features without writing a class.
+                </p>
+            </Expand>
+        </div>
+    );
+};
 
 export default App;
