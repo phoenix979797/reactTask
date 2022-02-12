bc63212cbe5c2158b57dfff8a0b5d2df18b2d50d
phoenix979797 <phoenix979797@proton.me>
2022-02-12 10:30:30 +0200

    das

--- ReactLessone/task1/src/Product.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import { Link } from "react-router-dom";
-
-const Product = ({ match }) => {
-    return (<div className="product">{match.params.productId}</div>)
-}
-
-
-export default Product
