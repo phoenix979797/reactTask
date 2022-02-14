d5275d6d4cf02c9790685d14ce4c8e21eeeccc0f
phoenix979797 <phoenix979797@proton.me>
2022-02-14 19:30:02 +0200

    sss

--- ReactLessone/task1/src/User.jsx
+++ ReactLessone/task1/src/User.jsx
@@ -1,25 +1,23 @@
-import React, { Component, useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
+import React, { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
 
-
-
-const User = ({ match }) => {
+const User = () => {
     const [userData, setUserData] = useState(null);
     const { userId } = useParams();
-    useEffect(() => {
 
+    useEffect(() => {
         fetch(`https://api.github.com/users/${userId}`)
-            .then(response => {
-                if (response.ok) {
-                    return response.json()
+            .then((res) => {
+                if (res.ok) {
+                    return res.json();
                 }
+
                 throw new Error();
             })
-            .then(data => {
-                setUserData(data)
-            })
-
-    }, [match.params.userId])
+            .then((userData) => {
+                setUserData(userData);
+            });
+    }, [userId]);
 
     if (!userData) {
         return null;
@@ -27,21 +25,15 @@ const User = ({ match }) => {
 
     const { name, location, avatar_url } = userData;
 
-    return <div className="user">
-        <img
-            alt="User Avatar"
-            src={avatar_url}
-            className="user__avatar"
-        />
-        <div className="user__info">
-            <span className="user__name">{name}</span>
-            <span className="user__location">{location}</span>
+    return (
+        <div className="user">
+            <img alt="User Avatar" src={avatar_url} className="user__avatar" />
+            <div className="user__info">
+                <span className="user__name">{name}</span>
+                <span className="user__location">{location}</span>
+            </div>
         </div>
-    </div>
-
-}
-
-
-
+    );
+};
 
 export default User;
--- ReactLessone/task1/webpack.config.js
+++ ReactLessone/task1/webpack.config.js
@@ -9,12 +9,12 @@ module.exports = (env, argv) => {
     entry: "./src/index.jsx",
     output: {
       filename: "bundle.js",
-      publicPath: '/'
     },
     module: {
       rules: [
         {
-          test: /.jsx?$/,
+          test: /.(js|jsx?)$/,
+          exclude: /node_modules/,
           use: ["babel-loader"],
         },
         {
@@ -27,6 +27,9 @@ module.exports = (env, argv) => {
         },
       ],
     },
+    resolve: {
+      extensions: [".js", ".jsx"],
+    },
     plugins: [
       new webpack.ProgressPlugin(),
       new CleanWebpackPlugin(),
@@ -34,14 +37,17 @@ module.exports = (env, argv) => {
         template: "./src/index.html",
       }),
     ],
-    resolve: {
-      extensions: [".js", ".jsx"],
-    },
     devServer: {
-      hot: true,
       historyApiFallback: true,
+      open: true,
+      hot: true,
+      port: 8080,
     },
   };
+
+  if (isProduction) {
+    config.plugins.push(new webpack.HotModuleReplacementPlugin());
+  }
 
   if (isProduction) {
     config.plugins.push(
