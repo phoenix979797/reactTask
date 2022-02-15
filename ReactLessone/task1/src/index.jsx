0698945c85e78ead7e8c51530f33a863032b671e
phoenix979797 <phoenix979797@proton.me>
2022-02-15 10:58:51 +0200

    ;;;
    ;;;;

--- ReactLessone/task1/src/index.jsx
+++ ReactLessone/task1/src/index.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.scss';
-import Dimensions from './Dimensions.jsx';
+import ConnectionStatus from './ConnectionStatus.jsx';
 
 
 
 const rootElement = document.querySelector('#root');
 
-ReactDOM.render(<Dimensions />, rootElement);
+ReactDOM.render(<ConnectionStatus />, rootElement);
 
